33fa4e460fb0c0221ea9e423aeeef3e0d363a4b7
MaryJo-github <jotjdal0215@gmail.com>
2023-06-26 00:54:33 +0900

    ✅ Test: DFS 문제풀이

--- BaekJoon/DFS/신기한 소수.py
+++ BaekJoon/DFS/신기한 소수.py
@@ -3,10 +3,12 @@ author: mary
 date: 23.06.23
 title: 신기한 소수
 """
-
+import sys
+input = sys.stdin.readline
 N = int(input())
+odd = [1, 3, 5, 7, 9]
 
-def isPrime(n):
+def is_prime(n):
     if n <= 1:
         return False
     for i in range(2, int(n**0.5)+1):
@@ -14,3 +16,13 @@ def isPrime(n):
             return False
     return True
 
+array2 = [2, 3, 5, 7]
+for _ in range(N-1):
+    array = array2
+    array2 = []
+    for i in array:
+        for o in odd:
+            if is_prime(10*i+o):
+                array2.append(10*i+o)
+
+print(*array2, end="\n")
--- README.md
+++ README.md
@@ -25,4 +25,4 @@
 |6월 22일| [스택과큐-절댓값힙](https://www.acmicpc.net/problem/11286) | ❌ | ✅ |
 |6월 22일| [DFS-연결요소의개수](https://www.acmicpc.net/problem/11724) | ❌ | ✅ |
 |6월 23일| [소수찾기-소수구하기](https://www.acmicpc.net/problem/1929) | ✅ |  |
-|6월 23일| [DFS-신기한소수](https://www.acmicpc.net/problem/2023) |  |  |
+|6월 25일| [DFS-신기한소수](https://www.acmicpc.net/problem/2023) | ✅ |  |
