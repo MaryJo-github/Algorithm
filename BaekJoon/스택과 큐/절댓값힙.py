9a5c189550ba99669f336b9c4062066a505882c7
MaryJo-github <jotjdal0215@gmail.com>
2023-07-10 01:21:28 +0900

    ✅ Test: 절대값힙 복습

--- BaekJoon/스택과 큐/절댓값힙.py
+++ BaekJoon/스택과 큐/절댓값힙.py
@@ -3,40 +3,40 @@ author: mary
 date: 23.06.22
 title: 절댓값힙
 """
+import heapq
+import sys
+input = sys.stdin.readline
 
-# from collections import deque
+N = int(input())
+queue = []
+result = []
+
+for _ in range(N):
+    v = int(input())
+    if v == 0:
+        try:
+            print(heapq.heappop(queue)[1])
+        except:
+            print(0)
+    else:
+        heapq.heappush(queue, (abs(v),v))
+
+
+# import heapq
 
 # N = int(input())
-# # array = [int(input()) for i in range(N)]
+# heap = []
 # result = []
-# sortD = deque()
 
 # for n in range(N):
 #     a = int(input())
 #     if a == 0:
-#         if len(sortD) == 0:
+#         if len(heap) == 0:
 #             result.append(0)
-#             break
-#         sign, value = sortD.popleft()
-#         result.append(sign * value)
+#             continue
+#         absValue, value = heapq.heappop(heap)
+#         result.append(value)
 #     else:
-#         sortD.append((, abs(a)))
+#         heapq.heappush(heap, (abs(a), a))
 
-import heapq
-
-N = int(input())
-heap = []
-result = []
-
-for n in range(N):
-    a = int(input())
-    if a == 0:
-        if len(heap) == 0:
-            result.append(0)
-            continue
-        absValue, value = heapq.heappop(heap)
-        result.append(value)
-    else:
-        heapq.heappush(heap, (abs(a), a))
-
-print(*result, sep="\n")
+# print(*result, sep="\n")
