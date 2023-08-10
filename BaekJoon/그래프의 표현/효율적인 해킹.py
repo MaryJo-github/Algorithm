235017de4598c4fca9d398eb5eaed7bab3f3878e
MaryJo-github <jotjdal0215@gmail.com>
2023-08-11 01:09:50 +0900

    ✅ 그래프의 표현 문제풀이

--- BaekJoon/그래프의 표현/효율적인 해킹.py
+++ BaekJoon/그래프의 표현/효율적인 해킹.py
@@ -14,36 +14,9 @@ for _ in range(M):
     a, b = map(int, input().split())
     graph[b].append(a)
     
-# def DFS(start):
-#     count = 0
-#     visited = [False] * (N+1)
-#     visited[0] = True
-
-#     visit_list = deque()
-#     visit_list.extend(graph[start])
-#     visited[start] = True
-
-#     while visit_list:
-#         node = visit_list.pop()        
-#         if visited[node] == False:
-#             visited[node] = True
-#             count += 1
-#             visit_list.extend(graph[node])
-#     return count
-
-# dfs_result = [-1]
-# for n in range(1, N+1):
-#     node, count = DFS(n)
-#     dfs_result.append(dfs_result[node]+count)
-
-# for n in range(N):
-#     if dfs_result[n] == max(dfs_result):
-#         print(n, end=" ")
-
 global_visited = [False] * (N+1)
 global_visited[0] = True 
 
-dfs_result = [-1] * (N+1)
 def DFS(start):
     count = 1
     visited = [False] * (N+1)
@@ -60,21 +33,13 @@ def DFS(start):
             visited[node] = True
             count += 1
             visit_list.extend(graph[node])
-            if global_visited[node] == False:
-                global_visited[node] = True
-                
-            if dfs_result[node] != -1:
-                return count + dfs_result[node]
-            else:
-                dfs_result[node] = DFS(node)
-                return count + dfs_result[node]
+            global_visited[node] = True
     return count
 
 result = []
 for n in range(1, N+1):
     if global_visited[n] == False:
-        result.append(DFS(n))
+        result.append((n,DFS(n)))
 
-for n in range(N):
-    if dfs_result[n] == max(dfs_result):
-        print(n, end=" ")
+for n in range(len(result)):
+    print(result[n][0], end=" ")
